ad988f2b339decf59b73a35596ba3c26bae5a8e1
Xiaoyang-Song <xysong@umich.edu>
2025-05-22 12:37:24 -0400

    Sync

---
+++ jobs/test.sh
@@ -0,0 +1,5 @@
+
+
+
+python train_ATD.py  --run_name cifar10 --model_type "fea" --training_type "adv" \
+    --in_dataset cifar10 --alpha 0.5 --batch_size 128 --num_epochs 20 --eps 0.0313 --attack_iters 10 
